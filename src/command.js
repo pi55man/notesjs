a32ed0b81d8f41df992b2808c39b31f0fec884f7
pi55man <cuinnv@gmail.com>
2024-10-06 18:32:10 +0530

    basic crud notes working.

--- src/command.js
+++ src/command.js
@@ -1,7 +1,8 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import fs from 'node:fs/promises'
+import {newNote, getAllNotes, removeNotes, removeAllNotes, findNotes} from './notes.js'
 
+//hidebin hides first two automatic arguments from process.argv
 yargs(hideBin(process.argv))
 	.command("new <note>", "create new note with content <note>", yargs => {
 		return yargs.positional('note', {
@@ -9,7 +10,9 @@ yargs(hideBin(process.argv))
 			description: "Content of note to create",
 		})
 	}, async (argv) => {
-		await fs.writeFile("/home/arin/code/notes/note1.txt", argv.note);
+			const tags = argv.tags ? argv.tags.split(",") : [];
+			const note = await newNote(argv.note,tags);
+			console.log('new note! ',note.id);
 	}).option('tags', {
 		alias: 't',
 		type: 'string',
@@ -17,18 +20,23 @@ yargs(hideBin(process.argv))
 	})
 	.command("all", "return all notes", () => { },
 		async (argv) => {
+				const notes = await getAllNotes();
+				console.log(notes);
 
 		})
 	.command("find <filter>", "get matching notes", yargs => {
 		return yargs.positional('filter', {
-			type: "string",
+				type: "string",
 			description: "search term to filter notes by",
 		})
 	}, async (argv) => {
+			const notes = await findNotes(argv.filter);
+			console.log(notes);
 
 	})
 	.command("clean", "deletes all notes", () => { }, async (argv) => {
-
+		removeAllNotes();
+		console.log("removed all notes")
 	})
 	.command("web [port]", "launch website to see notes", (yargs) => {
 		return yargs.positional("port", {
@@ -37,10 +45,22 @@ yargs(hideBin(process.argv))
 			description: 'port to bind on'
 		})
 	}, async (argv) => {
-
+		//TODO 
+	})
+    .command("remove <id>","delete note having id",(yargs)=>{
+		return yargs.positional("id",{
+				type:'number',
+				description:"id of the note to be deleted",
+		})
+	},async(argv)=>{
+		const id = await removeNotes(argv.id);
+		if(id){
+			console.log("removed! ", id)
+		}else{
+		    console.log("");
+		}
 	})
 	.demandCommand(1)
 	.parse();
 
 
-//hidebin hides first two automatic arguments from process.argv
